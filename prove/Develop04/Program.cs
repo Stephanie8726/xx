2c63a0b0ced7cb6ec37395ef6a19a4c4ef3d1ae6
Stephanie Dacullo <stephanie.laruga101@yahoo.com>
2023-11-04 19:52:43 -0800

    done program activity

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,9 +1,63 @@
 using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Linq;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Develop04 World!");
+        while (true)
+        {
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine("1. Breathing Activity");
+            Console.WriteLine("2. Reflection Activity");
+            Console.WriteLine("3. Listing Activity");
+            Console.WriteLine("4. Exit");
+            Console.Write("Select a choice from the menu: ");
+
+            if (int.TryParse(Console.ReadLine(), out int choice))
+            {
+                if (choice == 4)
+                {
+                    Console.WriteLine("Goodbye!");
+                    break;
+                }
+                else if (choice >= 1 && choice <= 3)
+                {
+                    Activity activity = null;
+
+                    if (choice == 1)
+                    {
+                        activity = new Breathing();
+                    }
+                    else if (choice == 2)
+                    {
+                        activity = new Reflection();
+                    }
+                    else if (choice == 3)
+                    {
+                        activity = new Listing();
+                    }
+
+                    if (activity != null)
+                    {
+                        activity.RunActivity();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid choice. Please try again.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid choice. Please try again.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a number.");
+            }
+        }
     }
 }
