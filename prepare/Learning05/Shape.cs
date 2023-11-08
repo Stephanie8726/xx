e925296651c99ca036684136c855959aa3e1bf53
Stephanie Dacullo <stephanie.laruga101@yahoo.com>
2023-11-08 00:39:47 -0800

    done learning05 shape

---
+++ prepare/Learning05/Shape.cs
@@ -0,0 +1,14 @@
+public class Shape
+{
+    public string Color { get; set; }
+
+    public Shape(string color)
+    {
+        Color = color;
+    }
+
+    public virtual double GetArea()
+    {
+        return 0;
+    }
+}
